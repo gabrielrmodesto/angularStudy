9c995fa5d65ec162564039500c63bac643e48a47
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-05-29 12:16:31 -0300

    Bind event

--- src/app/my-second-component/my-second-component.component.ts
+++ src/app/my-second-component/my-second-component.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-my-second-component',
-  templateUrl: './my-second-component.component.html',
-  styleUrls: ['./my-second-component.component.css']
+	selector: 'app-my-second-component',
+	templateUrl: './my-second-component.component.html',
+	styleUrls: ['./my-second-component.component.css'],
 })
 export class MySecondComponentComponent implements OnInit {
 	nome = 'Gabriel';
 	isVisible = true;
 	myValue = 4;
-	myList = [1,2,3,4,5];
+	myList = [1, 2, 3, 4, 5];
 	aluno = {
 		dados: {
-			nome: ''
-		}
+			nome: '',
+		},
+	};
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	handleClick(){
+		alert('Hi');
 	}
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
